f1026374c01c1552b1bd27d400da24e0e90d0fcb
Renan Cardoso <augustocardoso07@gmail.com>
2022-04-27 22:36:05 -0300

    sum modulo. Init of the course

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vedovelli-testing-course
 Repo for the Vedovelli testing course https://www.javascript.tv.br/
+
+Estou na aula 4 do primeiro módulo.
+
+TODO: 
+ - implement the parseQuery method with TDD
---
+++ module1/src/lib/calculator.js
@@ -0,0 +1,7 @@
+module.exports.sum = function(num1, num2) {
+    const int1 = parseInt(num1)
+    const int2 = parseInt(num2)
+
+    if (Number.isNaN(int1) || Number.isNaN(int2)) throw new Error('Invalid parameters')
+    return parseInt(num1) + parseInt(num2)
+}
